8e5279fca5517230b6d807be6c58bda340d86d6c
AtticusDavid <kajarga@eisenfinance.com>
2023-12-11 00:26:02 +0900

    feat: update leverage addresses

--- src/hardhat/constants.ts
+++ src/hardhat/constants.ts
@@ -61,9 +61,15 @@ export const multicall3Address: string =
   "0x39cA4391f1e462B452750D01Be0820573699A8d9";
 
 export const leveragerAddress: AddressMap = {
-  [`ethereumSepolia`]: `0x5e96230EB452667C44200F4b6e3D9F3FE962174c`,
-  [`polygonMumbai`]: `0x0020eb0e3d5E938129201d2e146748691Ee3261B`,
-  [`avalancheFuji`]: `0x5ccC154987a3c144F1F1fAD2F6Cf62ebAA716307`,
+  [`ethereumSepolia`]: `0x128a82abD1C6D83C951C723027D6D78b26c6107f`,
+  [`polygonMumbai`]: `0x30B69a7538CA8880fa9Ace4392cb2513dfE79A63`,
+  [`avalancheFuji`]: `0xAa82a246Ea9EBAb4AA132e6160217537bdfc35F1`,
+};
+
+export const propagatorAddress: AddressMap = {
+  [`ethereumSepolia`]: `0x3B8C94f5AB21A972453bB9A48D9d0c90c813B680`,
+  [`polygonMumbai`]: `0x73b5A1F764AED1431c626eB035307FCfb8F677b3`,
+  [`avalancheFuji`]: `0xd1280e14c5e30F431ccA989a43629E36D782Cb18`,
 };
 
 export const supportedNetworks = [
